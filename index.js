6a6a6390026d3560b897c8c33eb6df983ec6ff6e
Cuponk <cuponk64@gmail.com>
2022-12-24 10:35:51 -0800

    started main game function

--- index.js
+++ index.js
@@ -12,3 +12,35 @@ function getComputerChoice() {
     }
     return result;
   }
+
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper') {
+        return 'You lose!';
+    }
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors') {
+        return 'You win!';
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock') {
+        return 'You win!';
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors') {
+        return 'You lose!';
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock') {
+        return 'You lose!';
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper') {
+        return 'You Win!'
+    }
+    else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        return 'Tie!';
+    }
+  }
+
+  function game() {
+    let userScore = 0;
+    let cpuScore = 0;
+    while (userScore !== 5 || cpuScore !== 5) {
+        let input = prompt('Rock, Paper or Scissors')
+    }
+  }
